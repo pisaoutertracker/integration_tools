13d95039db88d293581e031981614317500bfd21
arizzi <andrea.rizzi@cern.ch>
2025-02-21 20:39:38 +0100

    add caen control and thermal camera

--- ui/main.py
+++ ui/main.py
@@ -1,10 +1,26 @@
 import integration_gui
-#inherit from integration_gui Ui_MainWindow
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPen
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QGraphicsScene
 from math import *
+
+# Set matplotlib backend before importing pyplot
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import paho.mqtt.client as mqtt
+import struct
+import numpy as np
+from caenGUI import caenGUI
+
+#MQTT_SERVER = "192.168.0.45"
+MQTT_SERVER = "test.mosquitto.org"
+MQTT_PATH = "/ar/thermal/image"
+
 class MainApp(integration_gui.Ui_MainWindow):
     def __init__(self, window):
         self.setupUi(window)    
@@ -15,6 +31,91 @@ class MainApp(integration_gui.Ui_MainWindow):
         self.fiberCB.addItems(fibers)
         self.powerCB.addItems(powers)
         self.draw_ring()
+        
+        # Setup thermal camera plotting
+        self.setup_thermal_plot()
+        
+        # Setup CAEN GUI
+        self.setup_caen_gui()
+        
+        # Setup MQTT client
+        self.setup_mqtt()
+
+    def setup_thermal_plot(self):
+        self.fig, (self.ax1, self.ax2) = plt.subplots(2, 1, figsize=(10, 8))
+        
+        # First plot: Image
+        self.im = self.ax1.imshow(np.random.rand(24, 32) * 30 + 10, cmap="plasma")
+        divider1 = make_axes_locatable(self.ax1)
+        self.cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(self.im, cax=self.cax1)
+
+        # Second plot: Trend
+        self.time = []
+        self.min_values = []
+        self.max_values = []
+        self.avg_values = []
+
+        self.line_min, = self.ax2.plot(self.time, self.min_values, label='Min')
+        self.line_max, = self.ax2.plot(self.time, self.max_values, label='Max')
+        self.line_avg, = self.ax2.plot(self.time, self.avg_values, label='Avg')
+        self.ax2.legend()
+
+        # Create canvas and add to layout
+        self.canvas = FigureCanvas(self.fig)
+        layout = QVBoxLayout(self.plotWidget)
+        layout.addWidget(self.canvas)
+
+    def setup_caen_gui(self):
+        self.caen = caenGUI()
+        layout = QVBoxLayout(self.caenWidget)
+        layout.addWidget(self.caen)
+
+    def setup_mqtt(self):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_mqtt_connect
+        self.client.on_message = self.on_mqtt_message
+        self.client.connect(MQTT_SERVER, 1883, 60)
+        self.client.loop_start()
+
+    def on_mqtt_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        client.subscribe(MQTT_PATH)
+
+    def on_mqtt_message(self, client, userdata, msg):
+        flo_arr = [
+            struct.unpack("f", msg.payload[i : i + 4])[0]
+            for i in range(0, len(msg.payload), 4)
+        ]
+        
+        # Update image plot
+        self.im.set_data(np.array(flo_arr).reshape(24, 32))
+        self.im.set_clim(18, 30)
+        
+        # Update trend plot
+        if len(self.time) == 0:
+            self.time.append(0)
+        else:
+            self.time.append(self.time[-1] + 1)
+            
+        self.min_values.append(min(flo_arr))
+        self.max_values.append(max(flo_arr))
+        self.avg_values.append(np.mean(flo_arr))
+        
+        # Keep maximum 600 values
+        if len(self.time) > 600:
+            self.time.pop(0)
+            self.min_values.pop(0)
+            self.max_values.pop(0)
+            self.avg_values.pop(0)
+
+        self.line_min.set_data(self.time, self.min_values)
+        self.line_max.set_data(self.time, self.max_values)
+        self.line_avg.set_data(self.time, self.avg_values)
+        self.ax2.relim()
+        self.ax2.autoscale_view()
+        
+        self.canvas.draw()
 
     def split_ring_and_position(self):
         ring_position = self.ringLE.text()
@@ -92,11 +193,13 @@ class MainApp(integration_gui.Ui_MainWindow):
         self.graphicsView.show()
 
 
-import sys
-if __name__ == '__main__':
-    
+def main():
     app = QApplication(sys.argv)
     window = QMainWindow()
     main_app = MainApp(window)
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    import sys
+    main()
